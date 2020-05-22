aef37cbf9a7b48471baaa824fb3d12f97d29ebd0
Aravinth Panch <me@aravinth.info>
2020-05-22 19:15:37 +0200

    Add stylesheets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, ThemeProvider, Header } from 'react-native-elements';
+// Author       : Aravinth Panch
+// Project      : Dentaria
+// Description  : Smart Dental Health Management System
+
+import React from "react";
+import { View, Alert } from "react-native";
+import { Button, ThemeProvider, Header } from "react-native-elements";
+import styles from "./styling";
 
 export default function DentariaApp() {
   return (
     <ThemeProvider>
       <Header
-        leftComponent={{ icon: 'menu', color: '#fff' }}
-        centerComponent={{ text: 'DENTARIA', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
+        leftComponent={{
+          icon: "menu",
+          color: "#fff",
+        }}
+        centerComponent={{
+          text: "DENTARIA",
+          style: {
+            color: "#fff",
+          },
+        }}
+        rightComponent={{
+          icon: "home",
+          color: "#fff",
+        }}
       />
-      <Button
-        title="PATIENT"
-        type="outline"
-      />
-      <Button
-        title="DENTIST"
-        type="outline"
-      />
+      <View style={styles.container}>
+        <Button
+          style={styles.button}
+          title="PATIENT"
+          type="outline"
+          onPress={() => Alert.alert("PATIENT")}
+        />
+        <Button
+          style={styles.button}
+          title="DENTIST"
+          type="outline"
+          onPress={() => Alert.alert("DENTIST")}
+        />
+      </View>
     </ThemeProvider>
   );
 }
---
+++ src/styling.js
@@ -0,0 +1,14 @@
+import { StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginVertical: 50,
+    marginHorizontal: 50,
+  },
+  button: {
+    paddingVertical: 10,
+  },
+});
+
+export default styles;
